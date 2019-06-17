e5058cc00fb78f07146e785602d88d23a71894bb
Babbar <sandeep.babbar@veris.in>
2019-06-17 15:46:13 +0530

    [Build] Storybook

---
+++ storybook-static/precache-manifest.d7843c1de16d45804c1a05943cfbc47d.js
@@ -0,0 +1,23 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aba3ed7aa5839ae8898b06f02e052616",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "278da866318df5e7e662",
+    "url": "main.css"
+  },
+  {
+    "url": "main.d0ed411a128d0930396e.bundle.js"
+  },
+  {
+    "url": "runtime~main.d0ed411a128d0930396e.bundle.js"
+  },
+  {
+    "revision": "79536324667490f72211",
+    "url": "vendors~main.css"
+  },
+  {
+    "url": "vendors~main.d0ed411a128d0930396e.bundle.js"
+  }
+]);
